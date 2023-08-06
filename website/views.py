3f2e6ca5ffcbcccb30fdd5fd409551d649359d8a
amireho3ein <amireho3ein@gmail.com>
2023-08-06 18:07:58 +0330

    Done Project Django CRM!

--- website/views.py
+++ website/views.py
@@ -1,43 +1,139 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .froms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
+from .models import Record
+
 
 def home(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        
-        user = authenticate(request ,username=username,password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, "You Have Been Logged In!")
-            return redirect('home')
-        else:
-            messages.success(request, "There Was An Error Logging In, Please Try Again...")
-            return redirect('home')
-    else:
-        return render(request, 'home.html', context={})
-    
-    
+	records = Record.objects.all()
+	# Check to see if logging in
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		# Authenticate
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, "You Have Been Logged In!")
+			return redirect('home')
+		else:
+			messages.success(request, "There Was An Error Logging In, Please Try Again...")
+			return redirect('home')
+	else:
+		return render(request, 'home.html', {'records':records})
+
+
 
 def logout_user(request):
-    logout(request)
-    messages.success(request, "You Have Logged Out!")
-    return redirect('home')
+	logout(request)
+	messages.success(request, "You Have Been Logged Out...")
+	return redirect('home')
+
 
 def register_user(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            messages.success(request, "You Have Successfully Registered! Welcome!")
-            return redirect('home')
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
+
+
+
+def customer_record(request, pk):
+	if request.user.is_authenticated:
+		# Look Up Records
+		customer_record = Record.objects.get(id=pk)
+		return render(request, 'record.html', {'customer_record':customer_record})
+	else:
+		messages.success(request, "You Must Be Logged In To View That Page...")
+		return redirect('home')
+
+
+
+def delete_record(request, pk):
+	if request.user.is_authenticated:
+		delete_it = Record.objects.get(id=pk)
+		delete_it.delete()
+		messages.success(request, "Record Deleted Successfully...")
+		return redirect('home')
+	else:
+		messages.success(request, "You Must Be Logged In To Do That...")
+		return redirect('home')
+
+
+def add_record(request):
+    if request.user.is_authenticated:
+        form = AddRecordForm(request.POST or None)
+        if request.method == "POST":
+            form = AddRecordForm(request.POST)
+            if form.is_valid():
+                first_name = form.cleaned_data['first_name']
+                last_name = form.cleaned_data['last_name']
+                email = form.cleaned_data['email']
+                phone = form.cleaned_data['phone']
+                address = form.cleaned_data['address']
+                city = form.cleaned_data['city']
+                state = form.cleaned_data['state']
+                zipcode = form.cleaned_data['zipcode']
+                add_record = Record.objects.create(
+                    first_name=first_name,
+                    last_name=last_name,
+                    email=email,
+                    phone=phone,
+                    address=address,
+                    city=city,
+                    state=state,
+                    zipcode=zipcode
+                )
+                messages.success(request, "Record Added...")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
     else:
-        form = SignUpForm()
-        return render(request, 'register.html', {'form':form})
-    return render(request, 'register.html', {'form':form})
+        messages.success(request, "You Must Be Logged In...")
+        return redirect('home')
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        if request.method == 'POST':
+            form = AddRecordForm(request.POST)
+            if form.is_valid():
+                current_record.first_name = form.cleaned_data['first_name']
+                current_record.last_name = form.cleaned_data['last_name']
+                current_record.email = form.cleaned_data['email']
+                current_record.phone = form.cleaned_data['phone']
+                current_record.address = form.cleaned_data['address']
+                current_record.city = form.cleaned_data['city']
+                current_record.state = form.cleaned_data['state']
+                current_record.zipcode = form.cleaned_data['zipcode']
+                current_record.save()
+                messages.success(request, "Record Has Been Updated!")
+                return redirect('home')
+        else:
+            form = AddRecordForm(initial={
+                'first_name': current_record.firstname,
+                'last_name': current_record.last_name,
+                'email': current_record.email,
+                'phone': current_record.phone,
+                'address': current_record.address,
+                'city': current_record.city,
+                'state': current_record.state,
+                'zipcode': current_record.zipcode
+            })
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.success(request, "You Must Be Logged In...")
+        return redirect('home')
